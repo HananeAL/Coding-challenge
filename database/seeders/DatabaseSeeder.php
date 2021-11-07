1c090742919ec14f0dc8c2ddf1323c2ebe6ae320
Hanane_Alkorr <hanane.alkorr@edu.uiz.ac.ma>
2021-11-07 15:51:05 +0100

    Add Data Base Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // set foreign key check to zero
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Product::truncate();
+        Category::truncate();
+        DB::table('category_product')->truncate();
+
+        Category::factory()->count(3)->create()->each(
+            function ($category) {
+                $category->parent()->associate(Category::inRandomOrder()->first()->id);
+                $category->save();
+            }
+        );
+
+        Product::factory()->count(3)->create()->each(
+            function ($product) {
+                $categories = Category::all()->random()->pluck('id');
+                $product->categories()->attach($categories);
+            }
+        );
     }
 }
