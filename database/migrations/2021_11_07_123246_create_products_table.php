8aafe20ed665c60f9a6e3953144ec04b4bd2aaeb
Hanane_Alkorr <hanane.alkorr@edu.uiz.ac.ma>
2021-11-12 22:19:53 +0100

    Update some types DB

--- database/migrations/2021_11_07_123246_create_products_table.php
+++ database/migrations/2021_11_07_123246_create_products_table.php
@@ -16,9 +16,9 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name', 30);
-            $table->string('description', 1000);
+            $table->longText('description', 1000);
             $table->float('price');
-            $table->string('image');
+            $table->longText('image');
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-// Route::resource('product', ProductController::class, ['only' => ['index', 'store', 'destroy']]);
+Route::resource('product', ProductController::class, ['only' => ['index', 'store', 'destroy']]);
 
 Route::resource('category', CategoryController::class, ['only' => ['index', 'store', 'destroy']]);
