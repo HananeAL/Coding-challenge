7b7872a6e5fa27bc005cfd3139b235d3e71de6ab
Hanane_Alkorr <hanane.alkorr@edu.uiz.ac.ma>
2021-11-12 22:18:33 +0100

    Add function make Image

---
+++ app/Utils/ImageFile.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Utils;
+
+use Illuminate\Support\Facades\Response;
+use Intervention\Image\Facades\Image;
+
+class ImageFile
+{
+    public static function makeImage($url)
+    {
+        $image = Image::make($url);
+        $img = self::base64($url);
+        (String) Response::make($image->encode('data-url'));
+
+        return $img;
+    }
+
+    public static function base64($base64)
+    {
+        // Get the image and convert into string
+        $img = file_get_contents($base64);
+        // Encode the image string data into base64
+        $data = base64_encode($img);
+
+        return $data;
+    }
+
+}
