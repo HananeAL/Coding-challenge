b553a3528a65b920ae648dce93135f6b348139a0
Hanane_Alkorr <hanane.alkorr@edu.uiz.ac.ma>
2021-11-12 20:00:30 +0100

    ProductValidator

---
+++ app/Validators/ProductValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Validators;
+
+use Illuminate\Support\Facades\Validator;
+
+class ProductValidator
+{
+    public function validate(array $data)
+    {
+        $validator = Validator::make($data, $this->rules());
+        $validator->validated();
+    }
+
+    private function rules()
+    {
+        return [
+            'name' => 'required|max:30|regex:/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/',
+            'description' => 'required|max:1000|string',
+            'price' => 'required|numeric',
+            'image' => 'required|base64file|base64image|base64max:1024',
+        ];
+    }
+}
